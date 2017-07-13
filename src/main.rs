1bf9183e7e232a291b5fead252ad8eb3dae8535f
Dan Munckton <dan@cultivatehq.com>
2017-07-13 21:40:24 +0100

    Use clap crate for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, ArgMatches};
 
 fn compound(principal: f64, annual_rate_percent: f64, period_in_years: i32, compounds_per_year: i32) -> f64 {
     let num_compounds = compounds_per_year * period_in_years;
@@ -26,12 +29,23 @@ fn compound_daily(principal: f64, annual_rate_percent: f64, period_in_years: i32
     compound(principal, annual_rate_percent, period_in_years, 365)
 }
 
+fn parse_args<'a>() -> ArgMatches<'a> {
+    App::new(crate_name!())
+        .about("Calculates compound interest")
+        .author(crate_authors!("\n"))
+        .version(crate_version!())
+        .arg(Arg::with_name("principal").required(true))
+        .arg(Arg::with_name("annual_rate_percent").required(true))
+        .arg(Arg::with_name("period_in_years").required(true))
+        .get_matches()
+}
+
 fn main() {
-    let mut args = env::args();
-    let _program = args.next();
-    let principal = args.next().expect("Missing arg").parse().expect("It all fucked up");
-    let annual_rate_percent = args.next().expect("Missing arg").parse().expect("It all fucked up");
-    let period_in_years = args.next().expect("Missing arg").parse().expect("It all fucked up");
+    let args = parse_args();
+
+    let principal = args.value_of("principal").unwrap().parse().unwrap();
+    let annual_rate_percent = args.value_of("annual_rate_percent").unwrap().parse().unwrap();
+    let period_in_years = args.value_of("period_in_years").unwrap().parse().unwrap();
 
     println!("£{:.3} Anually", compound_annually(principal, annual_rate_percent, period_in_years));
     println!("£{:.3} Monthly", compound_monthly(principal, annual_rate_percent, period_in_years));
