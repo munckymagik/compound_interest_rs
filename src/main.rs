a73577470ed231bc5bfcb7f5b99b61ce4d0a7b6c
Dan Munckton <dan@cultivatehq.com>
2017-07-13 21:19:42 +0100

    Brain dead simplest argument parsing I can think of

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn compound(principal: f64, annual_rate_percent: f64, period_in_years: i32, compounds_per_year: i32) -> f64 {
     let num_compounds = compounds_per_year * period_in_years;
 
@@ -25,9 +27,11 @@ fn compound_daily(principal: f64, annual_rate_percent: f64, period_in_years: i32
 }
 
 fn main() {
-    let principal = 20_000.0;
-    let annual_rate_percent = 1.0;
-    let period_in_years = 3;
+    let mut args = env::args();
+    let _program = args.next();
+    let principal = args.next().expect("Missing arg").parse().expect("It all fucked up");
+    let annual_rate_percent = args.next().expect("Missing arg").parse().expect("It all fucked up");
+    let period_in_years = args.next().expect("Missing arg").parse().expect("It all fucked up");
 
     println!("£{:.3} Anually", compound_annually(principal, annual_rate_percent, period_in_years));
     println!("£{:.3} Monthly", compound_monthly(principal, annual_rate_percent, period_in_years));
